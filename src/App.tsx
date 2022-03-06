93091cf6b00082d0ebd6f2e59e7ba9729a91e882
Lucas Prazeres <olucascostadp@gmail.com>
2022-03-06 10:55:39 -0300

    chore: config global styles and fonts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+
+
+export function App() {
+  return (
+    <div className="app">
+      <h1>dt.money</h1>
+      <GlobalStyle />
+    </div>
+  )
+}
+
+// 8257e6
