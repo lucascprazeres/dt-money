28481334d01a99e8894816a5c8620e950fabbb57
Lucas Prazeres <olucascostadp@gmail.com>
2022-03-06 11:09:02 -0300

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
-    <div className="app">
-      <h1>dt.money</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   )
 }
 
