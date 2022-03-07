6e802954eee9050bc7eb732d8a22a743a6d55f9c
Lucas Prazeres <olucascostadp@gmail.com>
2022-03-07 19:45:09 -0300

    feat: configure fake api with miragejs

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de site',
+          amount: 1200000,
+          type: 'deposit',
+          category: 'Venda',
+          createdAt: '12/04/2021'
+        },
+        {
+          id: 2,
+          title: 'Hamburguer',
+          amount: 5900,
+          type: 'withdraw',
+          category: 'Alimentação',
+          createdAt: '10/04/2021'
+        },
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
